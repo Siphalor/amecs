f69d08c205df2ca717d1e4a51b4a8e7bf45659a6
Siphalor <info@siphalor.de>
2021-04-22 19:49:38 +0200

    Display a text if no results are found

--- src/main/java/de/siphalor/amecs/gui/SearchFieldControlsListWidget.java
+++ src/main/java/de/siphalor/amecs/gui/SearchFieldControlsListWidget.java
@@ -3,22 +3,18 @@ package de.siphalor.amecs.gui;
 import de.siphalor.amecs.Amecs;
 import de.siphalor.amecs.compat.NMUKProxy;
 import de.siphalor.amecs.impl.duck.IKeyBindingEntry;
-import de.siphalor.amecs.mixin.ControlsListWidgetKeyBindingEntryAccessor;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.options.ControlsListWidget;
-import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 
@@ -38,116 +34,121 @@ public class SearchFieldControlsListWidget extends ControlsListWidget.Entry {
 		minecraft = minecraftClient;
 		TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
 		assert minecraft.currentScreen != null;
+
 		textFieldWidget = new TextFieldWidget(textRenderer, minecraft.currentScreen.width / 2 - 100, 0, 200, 20, new LiteralText(""));
 		textFieldWidget.setSuggestion(I18n.translate("amecs.search.placeholder"));
 		textFieldWidget.setChangedListener(searchText -> {
+			ControlsListWidget listWidget = null;
+			for (Element child : minecraftClient.currentScreen.children()) {
+				if (child instanceof ControlsListWidget) {
+					listWidget = (ControlsListWidget) child;
+					break;
+				}
+			}
+			assert listWidget != null;
+
 			if (searchText.isEmpty()) {
 				textFieldWidget.setSuggestion(I18n.translate("amecs.search.placeholder"));
 			} else {
 				textFieldWidget.setSuggestion("");
 			}
 
-			if (minecraftClient.currentScreen instanceof ControlsOptionsScreen) {
-				searchText = searchText.trim();
-				for(Element child : minecraftClient.currentScreen.children()) {
-					if(child instanceof ControlsListWidget) {
-						ControlsListWidget controlsListWidget = (ControlsListWidget) child;
-						controlsListWidget.setScrollAmount(0);
+			searchText = searchText.trim();
+			listWidget.setScrollAmount(0);
 
-						List<ControlsListWidget.Entry> children = controlsListWidget.children();
-						if (entries.isEmpty()) {
-							for (ControlsListWidget.Entry entry : children) {
-								if (entry instanceof ControlsListWidget.KeyBindingEntry) {
-									entries.add((ControlsListWidget.KeyBindingEntry) entry);
-								}
-							}
-							lastEntryCount = children.size();
-						}
-						int childrenCount = children.size();
-						if (childrenCount != lastEntryCount) {
-							Amecs.log(Level.INFO, "Controls search results changed externally - recompiling the list!");
-							try {
-								//noinspection JavaReflectionMemberAccess
-								Constructor<ControlsListWidget.KeyBindingEntry> c = ControlsListWidget.KeyBindingEntry.class.getDeclaredConstructor(
-										ControlsListWidget.class, KeyBinding.class, Text.class
-								);
-								c.setAccessible(true);
-								entries.clear();
-								KeyBinding[] keyBindings = minecraftClient.options.keysAll.clone();
-								Arrays.sort(keyBindings);
-								String lastCat = null;
-								ControlsListWidget.KeyBindingEntry entry;
-								lastEntryCount = 1;
-								for (KeyBinding keyBinding : keyBindings) {
-									if (!Objects.equals(lastCat, keyBinding.getCategory())) {
-										lastCat = keyBinding.getCategory();
-										children.add(controlsListWidget.new CategoryEntry(new TranslatableText(keyBinding.getCategory())));
-										lastEntryCount++;
-									}
-									entry = c.newInstance(controlsListWidget, keyBinding, new TranslatableText(keyBinding.getTranslationKey()));
-									children.add(entry);
-									entries.add(entry);
-									lastEntryCount++;
-								}
-							} catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
-								Amecs.log(Level.ERROR, "An unexpected exception occured during recompilation of controls list!");
-								e.printStackTrace();
-							}
-						}
-
-						children.clear();
-
-						children.add(this);
-
-						String keyFilter = null;
-						//noinspection ConstantConditions
-						int keyDelimiterPos = searchText.indexOf('=');
-						if(keyDelimiterPos == 0) {
-							keyFilter = searchText.substring(1).trim();
-							searchText = null;
-						} else if(keyDelimiterPos > 0) {
-							keyFilter = searchText.substring(keyDelimiterPos + 1).trim();
-							searchText = searchText.substring(0, keyDelimiterPos).trim();
-						}
-
-						final boolean nmuk = FabricLoader.getInstance().isModLoaded("nmuk");
-						String lastCat = null;
-						boolean lastMatched = false;
-						boolean includeCat = false;
-						lastEntryCount = 1;
-						for(ControlsListWidget.KeyBindingEntry entry : entries) {
-							KeyBinding binding = ((IKeyBindingEntry) entry).amecs$getKeyBinding();
-							if (nmuk && lastMatched && NMUKProxy.isAlternative(binding)) {
-								children.add(entry);
-								lastEntryCount++;
-								continue;
-							}
-
-							final String cat = binding.getCategory();
-							if (!cat.equals(lastCat)) {
-								includeCat = StringUtils.containsIgnoreCase(I18n.translate(cat), searchText);
-							}
-							if (
-									(
-											includeCat
-													|| searchText == null
-													|| StringUtils.containsIgnoreCase(I18n.translate(((IKeyBindingEntry) entry).amecs$getKeyBinding().getTranslationKey()), searchText)
-									) && Amecs.entryKeyMatches(entry, keyFilter)
-							) {
-								if (!cat.equals(lastCat)) {
-									children.add(controlsListWidget.new CategoryEntry(new TranslatableText(cat)));
-									lastCat = cat;
-									lastEntryCount++;
-								}
-								children.add(entry);
-								lastEntryCount++;
-								lastMatched = true;
-							} else {
-								lastMatched = false;
-							}
-						}
+			List<ControlsListWidget.Entry> children = listWidget.children();
+			if (entries.isEmpty()) {
+				for (ControlsListWidget.Entry entry : children) {
+					if (entry instanceof ControlsListWidget.KeyBindingEntry) {
+						entries.add((ControlsListWidget.KeyBindingEntry) entry);
 					}
 				}
+				lastEntryCount = children.size();
+			}
+			int childrenCount = children.size();
+			if (childrenCount != lastEntryCount) {
+				Amecs.log(Level.INFO, "Controls search results changed externally - recompiling the list!");
+				try {
+					//noinspection JavaReflectionMemberAccess
+					Constructor<ControlsListWidget.KeyBindingEntry> c = ControlsListWidget.KeyBindingEntry.class.getDeclaredConstructor(
+							ControlsListWidget.class, KeyBinding.class, Text.class
+					);
+					c.setAccessible(true);
+					entries.clear();
+					KeyBinding[] keyBindings = minecraftClient.options.keysAll.clone();
+					Arrays.sort(keyBindings);
+					String lastCat = null;
+					ControlsListWidget.KeyBindingEntry entry;
+					lastEntryCount = 1;
+					for (KeyBinding keyBinding : keyBindings) {
+						if (!Objects.equals(lastCat, keyBinding.getCategory())) {
+							lastCat = keyBinding.getCategory();
+							children.add(listWidget.new CategoryEntry(new TranslatableText(keyBinding.getCategory())));
+							lastEntryCount++;
+						}
+						entry = c.newInstance(listWidget, keyBinding, new TranslatableText(keyBinding.getTranslationKey()));
+						children.add(entry);
+						entries.add(entry);
+						lastEntryCount++;
+					}
+				} catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+					Amecs.log(Level.ERROR, "An unexpected exception occured during recompilation of controls list!");
+					e.printStackTrace();
+				}
+			}
+
+			children.clear();
+
+			children.add(this);
+
+			String keyFilter = null;
+			int keyDelimiterPos = searchText.indexOf('=');
+			if (keyDelimiterPos == 0) {
+				keyFilter = searchText.substring(1).trim();
+				searchText = null;
+			} else if (keyDelimiterPos > 0) {
+				keyFilter = searchText.substring(keyDelimiterPos + 1).trim();
+				searchText = searchText.substring(0, keyDelimiterPos).trim();
+			}
+
+			final boolean nmuk = FabricLoader.getInstance().isModLoaded("nmuk");
+			String lastCat = null;
+			boolean lastMatched = false;
+			boolean includeCat = false;
+			lastEntryCount = 1;
+			for (ControlsListWidget.KeyBindingEntry entry : entries) {
+				KeyBinding binding = ((IKeyBindingEntry) entry).amecs$getKeyBinding();
+				if (nmuk && lastMatched && NMUKProxy.isAlternative(binding)) {
+					children.add(entry);
+					lastEntryCount++;
+					continue;
+				}
+
+				final String cat = binding.getCategory();
+				if (!cat.equals(lastCat)) {
+					includeCat = StringUtils.containsIgnoreCase(I18n.translate(cat), searchText);
+				}
+				if (
+						(
+								includeCat
+										|| searchText == null
+										|| StringUtils.containsIgnoreCase(I18n.translate(((IKeyBindingEntry) entry).amecs$getKeyBinding().getTranslationKey()), searchText)
+						) && Amecs.entryKeyMatches(entry, keyFilter)
+				) {
+					if (!cat.equals(lastCat)) {
+						children.add(listWidget.new CategoryEntry(new TranslatableText(cat)));
+						lastCat = cat;
+						lastEntryCount++;
+					}
+					children.add(entry);
+					lastEntryCount++;
+					lastMatched = true;
+				} else {
+					lastMatched = false;
+				}
+			}
+			if (lastEntryCount <= 1) {
+				children.add(listWidget.new CategoryEntry(new TranslatableText(Amecs.MOD_ID + ".search.no_results")));
 			}
 		});
 	}
